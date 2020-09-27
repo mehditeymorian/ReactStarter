0a24a7f7b16e469165f8b2f918707f0d06f638ba
Mehdi <mehditeymorian322@gmail.com>
2020-09-27 19:53:11 +0330

    Add Hot Loader to App Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {hot} from 'react-hot-loader'
 import TodoList from "./todos/TodoList";
 import './App.css';
 
@@ -9,4 +10,4 @@ const App = () => (
   );
 
 
-export default App;
+export default hot(module)(App);
