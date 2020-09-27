04e727aba92833e6c98326db7c0ae5d407d207b7
Mehdi <mehditeymorian322@gmail.com>
2020-09-27 19:04:22 +0330

    Create TodoList Component

---
+++ src/todos/TodoList.js
@@ -0,0 +1,9 @@
+import React from "react";
+import TodoListItem from './TodoListItem';
+
+
+const TodoList = ({todos}) => (
+    <div className="list-wrapper">
+        {todos.map(todo => <TodoListItem todo={todo} />)}
+    </div>
+);
