51db8106aa2374493ba313f3cac6778f9d8d380b
Mehdi <mehditeymorian322@gmail.com>
2020-09-29 18:11:02 +0330

    setup Store for persisting data

--- src/store.js
+++ src/store.js
@@ -1,4 +1,7 @@
 import {createStore, combineReducers} from 'redux';
+import {persistReducer} from "redux-persist";
+import storage from 'redux-persist/lib/storage'
+import {autoMergeLevel2} from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import {todos} from "./todos/reducers";
 
 const reducers = {
@@ -6,6 +9,13 @@ const reducers = {
 
 };
 
-const rootReducer = combineReducers(reducers);
+const persistConfig = {
+    key: 'root',
+    storage,
+    stateReconciler : autoMergeLevel2
+};
 
-export const store = () => createStore(rootReducer);
+const rootReducer = combineReducers(reducers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const configureStore = () => createStore(persistedReducer);
